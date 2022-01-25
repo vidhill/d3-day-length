cf59f31c0ce1712c5d8633deb5b53ecec28bd2a9
David Hill <dh366k@att.com>
2022-01-25 00:37:17 +0000

    Add eslint congfig

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['vidhill'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {},
+    overrides: [
+        {
+            extends: ['vidhill/node'],
+            files: ['.eslintrc.js'],
+            env: {
+                node: true,
+            },
+        },
+    ],
+};
